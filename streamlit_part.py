d061b39f59bf26fd690910fbfedc873a5fb90e90
Nueri-ai <nurperi.bazlakunova01@universitadipavia.it>
2025-08-30 18:20:51 +0600

    Update streamlit_part.py

--- streamlit_part.py
+++ streamlit_part.py
@@ -2,7 +2,7 @@ import streamlit as st
 import joblib
 
 # Load model
-model = joblib.load(r"C:\Users\nurpe\Downloads\Bazlakunova_test_tasks_with_comments\Bazlakunova_test-tasks_3.1\best_m.pkl")
+model = joblib.load(r"C:\Users\nurpe\best_m.pkl")
 
 def predict_problems(overdues_max, overdues_mean, overdues_sum, n_loan_types, Age, gender, loan_year, loan_months):
     input_ = [[overdues_max, overdues_mean, overdues_sum, n_loan_types, Age, gender, loan_year, loan_months]]
@@ -25,3 +25,4 @@ if st.button("Predict customer credit rate type"):
     outcome = predict_problems(overdues_max, overdues_mean, overdues_sum, n_loan_types, Age, gender, loan_year, loan_months)
     mapp = {0:'Normal', 1:'Problematic'}
     st.write(f"Customer most likely is {mapp.get(outcome)}")
+
